7821cc23068c60cb0eb054540998368d2356d0d9
Frederik Bolding <frederik.bolding@gmail.com>
2020-02-10 15:52:30 +0100

    Basic print test

--- src/main/kotlin/com/egern/Main.kt
+++ src/main/kotlin/com/egern/Main.kt
@@ -2,13 +2,15 @@ package com.egern
 
 import MainLexer
 import MainParser
+import com.egern.ast.ArithExpr
 import com.egern.ast.BuildASTVisitor
 import com.egern.ast.Program
+import com.egern.ast.ReturnStmt
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 
 fun main(args: Array<String>) {
-    println("We in business! Input something + CTRL+D to run")
+    println("Egern Compiler v0.1 - We in business! Input something + CTRL+D to run")
     val input = CharStreams.fromStream(System.`in`)
     val lexer = MainLexer(input)
     val tokens = CommonTokenStream(lexer)
@@ -16,6 +18,12 @@ fun main(args: Array<String>) {
 
     val cst = parser.prog()
     val ast = BuildASTVisitor().visit(cst) as Program
+
+    val ret = ast.stmts[0] as ReturnStmt
+    val stmt = ret.expr as ArithExpr
+    println(stmt.lhs?.value)
+    println(stmt.op)
+    println(stmt.rhs?.value)
 }
 
 fun hello(): String {
