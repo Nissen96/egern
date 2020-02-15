b56998ab84d62e504ab5a6264116fde7e6057de3
Alexander Nissen <alexander@feldbech.dk>
2020-02-15 22:26:47 +0100

    Remove other print visits for now

--- src/main/kotlin/com/egern/Main.kt
+++ src/main/kotlin/com/egern/Main.kt
@@ -7,8 +7,6 @@ import com.egern.ast.Program
 import com.egern.symbols.SymbolVisitor
 import com.egern.types.TypeCheckingVisitor
 import com.egern.visitor.PrintProgramVisitor
-import com.egern.visitor.PrintSymbolTableVisitor
-import com.egern.visitor.PrintVisitor
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 
@@ -27,14 +25,6 @@ fun main(args: Array<String>) {
     if (doPrint) {
         val printProgramVisitor = PrintProgramVisitor(4)
         ast.accept(printProgramVisitor)
-
-        //val printVisitor = PrintVisitor()
-        //ast.accept(printVisitor)
-        //println()
-
-        //val printSymbolVisitor = PrintSymbolTableVisitor()
-        //ast.accept(printSymbolVisitor)
-        //println()
     }
 
     val symbolVisitor = SymbolVisitor()
