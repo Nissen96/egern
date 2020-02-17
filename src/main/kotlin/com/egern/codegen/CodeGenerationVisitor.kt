863482524e7f2f779d5f3b668aa005e3a2528daa
Frederik Bolding <frederik.bolding@gmail.com>
2020-02-17 15:48:11 +0100

    Added static link calculation

--- src/main/kotlin/com/egern/codegen/CodeGenerationVisitor.kt
+++ src/main/kotlin/com/egern/codegen/CodeGenerationVisitor.kt
@@ -1,15 +1,47 @@
 package com.egern.codegen
 
+import com.egern.ast.FuncDecl
 import com.egern.symbols.SymbolTable
 import com.egern.visitor.Visitor
 
-class CodeGenerationVisitor(val symbolTable: SymbolTable) : Visitor {
+class CodeGenerationVisitor(var symbolTable: SymbolTable) : Visitor {
     private val instructions = ArrayList<Instruction>();
+
+    companion object {
+        // CONSTANT OFFSETS FROM RBP
+        const val RSL_OFFSET = "-2"
+    }
 
     fun add(instruction: Instruction) {
         instructions.add(instruction)
     }
 
-    // TODO: Generate code
+    fun followStaticLink(diff: Int) {
+        add(
+            Instruction(
+                InstructionType.MOV, InstructionArg(RBP, Direct), InstructionArg(StaticLink, Direct),
+                comment = "Prepare to follow static link pointer"
+            )
+        )
+        for (i in 0..diff) {
+            add(
+                Instruction(
+                    InstructionType.MOV,
+                    InstructionArg(StaticLink, IndirectRelative(RSL_OFFSET)),
+                    InstructionArg(StaticLink, Direct),
+                    comment = "Following static link pointer"
+                )
+            )
+        }
+    }
 
+    override fun preVisit(funcDecl: FuncDecl) {
+        symbolTable = funcDecl.symbolTable
+    }
+
+    override fun postVisit(funcDecl: FuncDecl) {
+        symbolTable = funcDecl.symbolTable.parent!!
+    }
+
+    // TODO: Generate code
 }
