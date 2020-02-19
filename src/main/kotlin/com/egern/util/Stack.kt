a1cd44c35f463193ffb4d5697e1ac98662523452
Alexander Nissen <alexander@feldbech.dk>
2020-02-19 23:36:12 +0100

    Fix stack peek index bug

--- src/main/kotlin/com/egern/util/Stack.kt
+++ src/main/kotlin/com/egern/util/Stack.kt
@@ -6,5 +6,5 @@ fun <T> stackOf(vararg elements: T): Stack<T> = mutableListOf(*elements)
 fun <T> Stack<T>.push(item: T?) = item?.let { this.add(this.count(), it) }
 fun <T> Stack<T>.pop(): T? = if (this.count() > 0) this.removeAt(this.count() - 1) else null
 fun <T> Stack<T>.peek(): T? = if (this.count() > 0) this[this.count() - 1] else null
-fun <T> Stack<T>.peek(offset: Int): T? = if (this.count() > offset) this[this.count() - offset] else null
+fun <T> Stack<T>.peek(offset: Int): T? = if (this.count() > offset) this[this.count() - offset - 1] else null
 fun <T> Stack<T>.apply(func: (T) -> T) = if (this.count() > 0) this.push(func(this.pop()!!)) else null
