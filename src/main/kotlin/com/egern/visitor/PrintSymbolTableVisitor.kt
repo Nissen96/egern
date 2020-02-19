bfd937f09fb8bbe1b634d93d422ba667ff2eec29
Alexander Nissen <alexander@feldbech.dk>
2020-02-19 23:28:20 +0100

    More descriptive print symbol table visitor

--- src/main/kotlin/com/egern/visitor/PrintSymbolTableVisitor.kt
+++ src/main/kotlin/com/egern/visitor/PrintSymbolTableVisitor.kt
@@ -8,11 +8,11 @@ class PrintSymbolTableVisitor : Visitor {
     override fun preVisit(funcDecl: FuncDecl) {
         val scope = funcDecl.symbolTable.scope
         val symbol = funcDecl.symbolTable.lookup(funcDecl.id)
-        printSymbolLine(symbol, scope - 1)
+        printSymbolLine(symbol, scope - 1, "Local variables", funcDecl.variableCount)
 
         for (param in funcDecl.params) {
             val paramSymbol = funcDecl.symbolTable.lookup(param)
-            printSymbolLine(paramSymbol, scope)
+            printSymbolLine(paramSymbol, scope, "Offset", paramSymbol?.info as Int)
         }
     }
 
@@ -20,11 +20,11 @@ class PrintSymbolTableVisitor : Visitor {
         val scope = varDecl.symbolTable.scope
         for (id in varDecl.ids) {
             val symbol = varDecl.symbolTable.lookup(id)
-            printSymbolLine(symbol, scope)
+            printSymbolLine(symbol, scope, "Offset", symbol?.info as Int)
         }
     }
 
-    private fun printSymbolLine(symbol: Symbol<*>?, scope: Int) {
-        println("${symbol?.type}\t at scope $scope: ${symbol?.id}")
+    private fun printSymbolLine(symbol: Symbol<*>?, scope: Int, infoText: String, infoValue: Int) {
+        println("${symbol?.type} at scope $scope: ${symbol?.id}\n    $infoText: $infoValue")
     }
 }
