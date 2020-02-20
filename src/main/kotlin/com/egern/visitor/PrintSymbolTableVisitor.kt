f1d1311cd62656ea5be79ff256075750cedf0564
Alexander Nissen <alexander@feldbech.dk>
2020-02-20 08:48:35 +0100

    Add main scope to print symbol table visitor

--- src/main/kotlin/com/egern/visitor/PrintSymbolTableVisitor.kt
+++ src/main/kotlin/com/egern/visitor/PrintSymbolTableVisitor.kt
@@ -2,17 +2,20 @@ package com.egern.visitor
 
 import com.egern.ast.*
 import com.egern.symbols.Symbol
-import com.egern.symbols.SymbolType
 
 class PrintSymbolTableVisitor : Visitor {
+    override fun preVisit(program: Program) {
+        println("Main Scope - local variables: ${program.variableCount}")
+    }
+
     override fun preVisit(funcDecl: FuncDecl) {
         val scope = funcDecl.symbolTable.scope
         val symbol = funcDecl.symbolTable.lookup(funcDecl.id)
-        printSymbolLine(symbol, scope - 1, "Local variables", funcDecl.variableCount)
+        printSymbolLine(symbol, scope - 1, "local variables", funcDecl.variableCount)
 
         for (param in funcDecl.params) {
             val paramSymbol = funcDecl.symbolTable.lookup(param)
-            printSymbolLine(paramSymbol, scope, "Offset", paramSymbol?.info as Int)
+            printSymbolLine(paramSymbol, scope, "offset", paramSymbol?.info as Int)
         }
     }
 
@@ -20,11 +23,11 @@ class PrintSymbolTableVisitor : Visitor {
         val scope = varDecl.symbolTable.scope
         for (id in varDecl.ids) {
             val symbol = varDecl.symbolTable.lookup(id)
-            printSymbolLine(symbol, scope, "Offset", symbol?.info as Int)
+            printSymbolLine(symbol, scope, "offset", symbol?.info as Int)
         }
     }
 
     private fun printSymbolLine(symbol: Symbol<*>?, scope: Int, infoText: String, infoValue: Int) {
-        println("${symbol?.type} at scope $scope: ${symbol?.id}\n    $infoText: $infoValue")
+        println("${symbol?.type} '${symbol?.id}' at scope $scope - $infoText: $infoValue")
     }
 }
