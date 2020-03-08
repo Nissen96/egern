7a6ebf9f409105f95ea99f18c4c6f90b4296db7e
Jacob Vesti Moeslund Nielsen <jvmnielsen@protonmail.com>
2020-03-08 17:06:21 +0100

    Build and test scripts for MacOS

---
+++ src/test/egern/run-macOS.sh
@@ -0,0 +1,8 @@
+./compile $1 > ko.nasm || exit 1
+nasm -f macho64 ko.nasm || exit 1
+ld ko.o -lSystem || exit 1
+rm ko.nasm >/dev/null 2>&1
+rm ko.o >/dev/null 2>&1
+./a.out || exit 1
+rm a.out >/dev/null 2>&1
+exit 0
---
+++ src/test/egern/test-macOS.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Find expected output
+filename="${1##*/}"
+expected="expected/${filename%.*}"
+
+# Compile and run
+./compile $1 > ko.nasm || exit 1
+nasm -f macho64 ko.nasm || exit 1
+ld ko.o -lSystem || exit 1
+rm ko.nasm >/dev/null 2>&1
+rm ko.o >/dev/null 2>&1
+./a.out > result 2>&1 || exit 1
+rm a.out >/dev/null 2>&1
+
+# Compare results
+diff result $expected >/dev/null 2>&1
+if [ $? -gt 0 ]
+then
+    echo "Expected:"
+    cat $expected
+    echo "Actual:"
+    cat result
+    rm result >/dev/null 2>&1
+    exit 1
+fi
+
+rm result >/dev/null 2>&1
+exit 0
