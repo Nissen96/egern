4c28bbe535ad2721d1c5d714fa5d17d8e2e309f3
Alexander Nissen <alexander@feldbech.dk>
2020-04-01 00:03:53 +0200

    Apply new forEach on method calls and object instantiation

--- src/main/kotlin/com/egern/ast/MethodCall.kt
+++ src/main/kotlin/com/egern/ast/MethodCall.kt
@@ -1,18 +1,13 @@
 package com.egern.ast
 
+import com.egern.util.forEach
 import com.egern.visitor.Visitor
 
 class MethodCall(val objectId: String, val methodId: String, val args: List<Expr>, lineNumber: Int, charPosition: Int) :
     Expr(lineNumber, charPosition) {
     override fun accept(visitor: Visitor) {
         visitor.preVisit(this)
-        if (args.isNotEmpty()) {
-            args.dropLast(1).map {
-                it.accept(visitor)
-                visitor.midVisit(this)
-            }
-            args.last().accept(visitor)
-        }
+        args.forEach({ it.accept(visitor) }, doBetween = { visitor.midVisit(this) })
         visitor.postVisit(this)
     }
 }
