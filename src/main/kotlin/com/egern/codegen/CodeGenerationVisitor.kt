9746d41ebfd80a4657314e0dc27fb526290e6335
Frederik Bolding <frederik.bolding@gmail.com>
2020-04-20 14:49:06 +0200

    Flipped jump

--- src/main/kotlin/com/egern/codegen/CodeGenerationVisitor.kt
+++ src/main/kotlin/com/egern/codegen/CodeGenerationVisitor.kt
@@ -988,7 +988,7 @@ class CodeGenerationVisitor(private var symbolTable: SymbolTable, private val cl
             )
             add(
                 Instruction(
-                    InstructionType.JE,
+                    InstructionType.JNE,
                     InstructionArg(Memory(falseLabel), Direct)
                 )
             )
