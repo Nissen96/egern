714e28680b10e2cec9cdefb6dd5db0ac46886d21
Jacob Vesti Moeslund Nielsen <jvmnielsen@gmail.com>
2020-04-26 19:05:36 +0200

    Update output-file extension; changed test character encoding to ascii

--- src/test/windows/compile.cmd
+++ src/test/windows/compile.cmd
@@ -1,2 +1,2 @@
 @echo off
-call ..\..\.\gradlew --quiet -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false run -p ../../ --args="-q" < %1 > ko.nasm
+call ..\..\.\gradlew --quiet -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false run -p ../../ --args="-q" < %1 > ko.asm
--- src/test/windows/run.cmd
+++ src/test/windows/run.cmd
@@ -1,8 +1,8 @@
 @echo off
 call windows/compile.cmd %1 
-call nasm -f win64 ko.nasm -o ko.obj 
-call link ko.obj /entry:_main /subsystem:console /defaultlib:ucrt.lib /defaultlib:legacy_stdio_definitions.lib /libpath:"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\lib\onecore\x64" /libpath:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\ucrt\x64" > NUL 2>&1
-del ko.nasm
+call nasm -f win64 ko.asm -o ko.obj 
+call link ko.obj /entry:_main /subsystem:console /defaultlib:ucrt.lib /defaultlib:legacy_stdio_definitions.lib /LARGEADDRESSAWARE:NO /libpath:"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\lib\onecore\x64" /libpath:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\ucrt\x64" > NUL 2>&1
+del ko.asm
 del ko.obj
 call ko.exe
 del ko.exe
