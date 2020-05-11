651f30ded15bc185f5e4fbb85959a7e5d76ee452
Alexander Nissen <alexander@feldbech.dk>
2020-05-11 18:33:41 +0200

    Update runtime - forward, chase and more

--- src/main/runtime/runtime.c
+++ src/main/runtime/runtime.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // STACK FRAME - CONSTANT OFFSETS FROM RBP
 const long int LOCAL_VAR_OFFSET = 4;
@@ -19,8 +20,19 @@ const long int DATA_OFFSET = 2;
 long int* heap_pointer;
 long int* from_space;
 long int* to_space;
+long int heap_size;
 long int* current_heap_pointer;
+long int* current_to_space_pointer;
 
+
+void print_heap() {
+    printf("       FROM-SPACE       TO-SPACE\n");
+    printf("       (%d)      (%d)\n", from_space, to_space);
+    for (int i = 0; i < heap_size; ++i) {
+        printf("%4d: %10d %15d\n", i, from_space[i], to_space[i]);
+    }
+    printf("\n\n");
+}
 
 void set_bitmap(long int* ptr, int* bitmap) {
     unsigned char* b = (unsigned char*) ptr;
@@ -47,50 +59,102 @@ void swap(long int* a, long int* b) {
     b = temp;
 }
 
-long int* getPointerField(long int* ptr, int k) {
+void check_pointer(long int* ptr) {
+    for (int i = -16; i < 16; ++i) {
+        printf("Index %d: %d\n", i, ptr[i]);
+    }
+}
+
+int in_to_space(long int* ptr) {
+    return ptr != NULL && ptr >= to_space && ptr < to_space + heap_size;
+}
+
+int in_from_space(long int* ptr) {
+    return ptr != NULL && ptr >= from_space && ptr < from_space + heap_size;
+}
+
+long int* get_pointer_field(long int* ptr, int k) {
     // Get bitmap
     long int* fields = ptr + DATA_OFFSET;
     long int num_fields = *(ptr + SIZE_INFO_OFFSET);
     int bitmap[64];
-    set_bitmap(ptr - BITMAP_OFFSET, bitmap);
+    set_bitmap(ptr + BITMAP_OFFSET, bitmap);
 
     // Find k'th pointer field
     int pointers_found = 0;
     for (int i = 0; i < num_fields; ++i) {
-        int is_pointer = bitmap[i];
+        int is_pointer = bitmap[num_fields - i - 1];
         if (is_pointer) {
-            long int* field = (long int*) fields[-num_fields + i + 1];
+            long int* field = (long int*) fields[i];
             if (pointers_found == k) return field;
 
             pointers_found++;
         }
     }
+    return NULL;
 }
 
-long int* chase(long int* ptr) {
+void chase(long int* ptr) {
+    int bitmap[64];
 
+    // Handle entire struct at pointer depth-first
+    while (ptr != NULL) {
+        int num_fields = *(ptr + SIZE_INFO_OFFSET);
+        set_bitmap(ptr + BITMAP_OFFSET, bitmap);
+
+        // Copy everything at ptr to to-space
+        long int* to_ptr = current_to_space_pointer;
+        current_to_space_pointer += DATA_OFFSET + num_fields;
+        memcpy(to_ptr, ptr, (num_fields + 2) * 8);
+
+        // Find last field still pointing to from-space
+        long int* next_from_space_field = NULL;
+        long int* next_field = NULL;
+        int field_num = 0;
+        do {
+            long int* current_field = next_field;
+            next_field = get_pointer_field(to_ptr, field_num);
+            ++field_num;
+
+            if (in_from_space(current_field)) {
+                set_bitmap(current_field + BITMAP_OFFSET, bitmap);
+                if (bitmap[0] == 1 && !in_to_space((long int*) current_field[2])) {
+                    next_from_space_field = current_field;
+                }
+            }
+        } while (next_field != NULL);
+
+        ptr[2] = (long int) to_ptr;
+        ptr = next_from_space_field;
+    }
 }
 
 long int* forward(long int* ptr) {
-    // Points to from-space
-    if (ptr < to_space) {
-        long int* field = getPointerField(ptr, 0);
-        if (field < to_space) {
-            chase(ptr);
-        }
-        return field;
+    // Nothing to do if already in to-space
+    if (in_to_space(ptr)) {
+        return ptr;
     }
-    return ptr;
+
+    // End condition: first pointer field is already in to-space (fields handled in reverse)
+    long int* field = get_pointer_field(ptr, 0);
+    if (field != NULL && !in_to_space(field)) {
+        chase(ptr);
+    }
+    return field;
 }
 
 void visit_pointer_vars(long int num_vars, int* bitmap, long int* variables) {
     for (int i = 0; i < num_vars; ++i) {
-        int is_pointer = bitmap[i];
-
+        int is_pointer = bitmap[num_vars - i - 1];
         if (is_pointer) {
-            long int* variable = (long int*) variables[-num_vars + i + 1];
-            printf("Var %d = %d is pointer\n", i, variable);
-            forward(variable);
+            long int* variable = (long int*) variables[-i];
+            if (variable < from_space || variable >= to_space) {
+                return;  // reached uninitialized variable
+            }
+            printf("Var %d = %d (pointer)\n", i, variable);
+
+            // Forward variable to to-space and store forward address
+            variables[-i] = (long int) forward(variable);
         }
     }
 }
@@ -115,7 +179,11 @@ void scan_stack_frame(long int* rbp) {
     int i, j;
     for (i = 0; i < num_params; i++) param_bitmap[i] = bitmap[i];
     for (j = 0; j < num_vars; j++) var_bitmap[j] = bitmap[i++];
+
+    //check_pointer(rbp - LOCAL_VAR_OFFSET);
     visit_pointer_vars(num_vars, var_bitmap, rbp - LOCAL_VAR_OFFSET);
+
+    printf("\n");
 }
 
 void collect_garbage(long int* rbp) {
@@ -125,35 +193,30 @@ void collect_garbage(long int* rbp) {
         scan_stack_frame(rbp);
         rbp = (long int*) *rbp;
     }
-    printf("Done!\n");
+    current_heap_pointer = current_to_space_pointer;
 }
 
-long int* allocate_heap(long int size, long int heap_size, long int* rbp) {
+long int* allocate_heap(long int size, long int* rbp) {
     from_space = heap_pointer;
     to_space = from_space + heap_size;
 
     if (current_heap_pointer >= to_space) {
         swap(from_space, to_space);
     }
-
-    /*printf("Heap pointer: %d\n", from_space);
-    printf("Heap size:    %d\n", heap_size);
-    printf("Current:      %d\n", current_heap_pointer);
-    printf("Allocation:   %d\n", size);
-
-    printf("New current:  %d\n", current_heap_pointer + size);
-    printf("Heap 1 end:   %d\n\n", from_space + heap_size);*/
+    current_to_space_pointer = to_space;
 
     // Run garbage collection if heap limit is reached
     if (current_heap_pointer + size > from_space + heap_size) {
+        print_heap();
         collect_garbage(rbp);
+        print_heap();
+
+        // Out of memory
+        if (current_heap_pointer + size > to_space + heap_size) {
+            fprintf(stderr, "Out of memory\n");
+            exit(1);
+        }
     }
 
-    // Out of memory
-    if (current_heap_pointer + size > from_space + heap_size) {
-        fprintf(stderr, "Out of memory\n");
-        exit(1);
-    }
-
-    return heap_pointer;
+    return current_heap_pointer;
 }
--- src/main/runtime/runtime_att.asm
+++ src/main/runtime/runtime_att.asm
@@ -18,6 +18,58 @@ BITMAP_OFFSET:
         .quad   1
 DATA_OFFSET:
         .quad   2
+.LC0:
+        .string "       FROM-SPACE       TO-SPACE"
+.LC1:
+        .string "       (%d)      (%d)\n"
+.LC2:
+        .string "%4d: %10d %15d\n"
+.LC3:
+        .string "\n"
+print_heap:
+        pushq   %rbp
+        movq    %rsp, %rbp
+        subq    $16, %rsp
+        movl    $.LC0, %edi
+        call    puts
+        movq    to_space(%rip), %rdx
+        movq    from_space(%rip), %rax
+        movq    %rax, %rsi
+        movl    $.LC1, %edi
+        movl    $0, %eax
+        call    printf
+        movl    $0, -4(%rbp)
+        jmp     .L2
+.L3:
+        movq    to_space(%rip), %rax
+        movl    -4(%rbp), %edx
+        movslq  %edx, %rdx
+        salq    $3, %rdx
+        addq    %rdx, %rax
+        movq    (%rax), %rcx
+        movq    from_space(%rip), %rax
+        movl    -4(%rbp), %edx
+        movslq  %edx, %rdx
+        salq    $3, %rdx
+        addq    %rdx, %rax
+        movq    (%rax), %rdx
+        movl    -4(%rbp), %eax
+        movl    %eax, %esi
+        movl    $.LC2, %edi
+        movl    $0, %eax
+        call    printf
+        addl    $1, -4(%rbp)
+.L2:
+        movl    -4(%rbp), %eax
+        movslq  %eax, %rdx
+        movq    heap_size(%rip), %rax
+        cmpq    %rax, %rdx
+        jl      .L3
+        movl    $.LC3, %edi
+        call    puts
+        nop
+        leave
+        ret
 set_bitmap:
         pushq   %rbp
         movq    %rsp, %rbp
@@ -26,11 +78,11 @@ set_bitmap:
         movq    -40(%rbp), %rax
         movq    %rax, -16(%rbp)
         movl    $7, -4(%rbp)
-        jmp     .L2
-.L5:
+        jmp     .L5
+.L8:
         movl    $7, -8(%rbp)
-        jmp     .L3
-.L4:
+        jmp     .L6
+.L7:
         movl    -4(%rbp), %eax
         movslq  %eax, %rdx
         movq    -16(%rbp), %rax
@@ -54,20 +106,20 @@ set_bitmap:
         movzbl  -17(%rbp), %eax
         movl    %eax, (%rdx)
         subl    $1, -8(%rbp)
-.L3:
+.L6:
         cmpl    $0, -8(%rbp)
-        jns     .L4
+        jns     .L7
         subl    $1, -4(%rbp)
-.L2:
+.L5:
         cmpl    $0, -4(%rbp)
-        jns     .L5
+        jns     .L8
         nop
         nop
         popq    %rbp
         ret
-.LC0:
+.LC4:
         .string "Bitmap:         "
-.LC1:
+.LC5:
         .string "%d"
 print_bitmap:
         pushq   %rbp
@@ -75,12 +127,12 @@ print_bitmap:
         subq    $32, %rsp
         movl    %edi, -20(%rbp)
         movq    %rsi, -32(%rbp)
-        movl    $.LC0, %edi
+        movl    $.LC4, %edi
         movl    $0, %eax
         call    printf
         movl    $0, -4(%rbp)
-        jmp     .L7
-.L8:
+        jmp     .L10
+.L11:
         movl    -4(%rbp), %eax
         cltq
         leaq    0(,%rax,4), %rdx
@@ -88,14 +140,14 @@ print_bitmap:
         addq    %rdx, %rax
         movl    (%rax), %eax
         movl    %eax, %esi
-        movl    $.LC1, %edi
+        movl    $.LC5, %edi
         movl    $0, %eax
         call    printf
         addl    $1, -4(%rbp)
-.L7:
+.L10:
         movl    -4(%rbp), %eax
         cmpl    -20(%rbp), %eax
-        jl      .L8
+        jl      .L11
         movl    $10, %edi
         call    putchar
         nop
@@ -115,7 +167,80 @@ swap:
         nop
         popq    %rbp
         ret
-getPointerField:
+.LC6:
+        .string "Index %d: %d\n"
+check_pointer:
+        pushq   %rbp
+        movq    %rsp, %rbp
+        subq    $32, %rsp
+        movq    %rdi, -24(%rbp)
+        movl    $-16, -4(%rbp)
+        jmp     .L14
+.L15:
+        movl    -4(%rbp), %eax
+        cltq
+        leaq    0(,%rax,8), %rdx
+        movq    -24(%rbp), %rax
+        addq    %rdx, %rax
+        movq    (%rax), %rdx
+        movl    -4(%rbp), %eax
+        movl    %eax, %esi
+        movl    $.LC6, %edi
+        movl    $0, %eax
+        call    printf
+        addl    $1, -4(%rbp)
+.L14:
+        cmpl    $15, -4(%rbp)
+        jle     .L15
+        nop
+        nop
+        leave
+        ret
+in_to_space:
+        pushq   %rbp
+        movq    %rsp, %rbp
+        movq    %rdi, -8(%rbp)
+        cmpq    $0, -8(%rbp)
+        je      .L17
+        movq    to_space(%rip), %rax
+        cmpq    %rax, -8(%rbp)
+        jb      .L17
+        movq    to_space(%rip), %rax
+        movq    heap_size(%rip), %rdx
+        salq    $3, %rdx
+        addq    %rdx, %rax
+        cmpq    %rax, -8(%rbp)
+        jnb     .L17
+        movl    $1, %eax
+        jmp     .L19
+.L17:
+        movl    $0, %eax
+.L19:
+        popq    %rbp
+        ret
+in_from_space:
+        pushq   %rbp
+        movq    %rsp, %rbp
+        movq    %rdi, -8(%rbp)
+        cmpq    $0, -8(%rbp)
+        je      .L21
+        movq    from_space(%rip), %rax
+        cmpq    %rax, -8(%rbp)
+        jb      .L21
+        movq    from_space(%rip), %rax
+        movq    heap_size(%rip), %rdx
+        salq    $3, %rdx
+        addq    %rdx, %rax
+        cmpq    %rax, -8(%rbp)
+        jnb     .L21
+        movl    $1, %eax
+        jmp     .L23
+.L21:
+        movl    $0, %eax
+.L23:
+        popq    %rbp
+        ret
+get_pointer_field:
         pushq   %rbp
         movq    %rsp, %rbp
         subq    $320, %rsp
@@ -133,9 +258,7 @@ getPointerField:
         movq    (%rax), %rax
         movq    %rax, -24(%rbp)
         movl    $1, %eax
-        salq    $3, %rax
-        negq    %rax
-        movq    %rax, %rdx
+        leaq    0(,%rax,8), %rdx
         movq    -312(%rbp), %rax
         addq    %rax, %rdx
         leaq    -304(%rbp), %rax
@@ -144,18 +267,20 @@ getPointerField:
         call    set_bitmap
         movl    $0, -4(%rbp)
         movl    $0, -8(%rbp)
-        jmp     .L11
-.L15:
+        jmp     .L25
+.L29:
         movl    -8(%rbp), %eax
         cltq
+        movq    -24(%rbp), %rdx
+        subq    %rax, %rdx
+        movq    %rdx, %rax
+        subq    $1, %rax
         movl    -304(%rbp,%rax,4), %eax
         movl    %eax, -28(%rbp)
         cmpl    $0, -28(%rbp)
-        je      .L12
+        je      .L26
         movl    -8(%rbp), %eax
         cltq
-        subq    -24(%rbp), %rax
-        addq    $1, %rax
         leaq    0(,%rax,8), %rdx
         movq    -16(%rbp), %rax
         addq    %rdx, %rax
@@ -163,57 +288,151 @@ getPointerField:
         movq    %rax, -40(%rbp)
         movl    -4(%rbp), %eax
         cmpl    -316(%rbp), %eax
-        jne     .L13
+        jne     .L27
         movq    -40(%rbp), %rax
-        jmp     .L10
-.L13:
+        jmp     .L30
+.L27:
         addl    $1, -4(%rbp)
-.L12:
+.L26:
         addl    $1, -8(%rbp)
-.L11:
+.L25:
         movl    -8(%rbp), %eax
         cltq
         cmpq    %rax, -24(%rbp)
-        jg      .L15
-.L10:
+        jg      .L29
+        movl    $0, %eax
+.L30:
         leave
         ret
 chase:
         pushq   %rbp
         movq    %rsp, %rbp
-        movq    %rdi, -8(%rbp)
+        subq    $320, %rsp
+        movq    %rdi, -312(%rbp)
+        jmp     .L32
+.L35:
+        movl    $0, %eax
+        leaq    0(,%rax,8), %rdx
+        movq    -312(%rbp), %rax
+        addq    %rdx, %rax
+        movq    (%rax), %rax
+        movl    %eax, -24(%rbp)
+        movl    $1, %eax
+        leaq    0(,%rax,8), %rdx
+        movq    -312(%rbp), %rax
+        addq    %rax, %rdx
+        leaq    -304(%rbp), %rax
+        movq    %rax, %rsi
+        movq    %rdx, %rdi
+        call    set_bitmap
+        movq    current_to_space_pointer(%rip), %rax
+        movq    %rax, -32(%rbp)
+        movq    current_to_space_pointer(%rip), %rax
+        movl    -24(%rbp), %edx
+        movslq  %edx, %rdx
+        movl    $2, %ecx
+        addq    %rcx, %rdx
+        salq    $3, %rdx
+        addq    %rdx, %rax
+        movq    %rax, current_to_space_pointer(%rip)
+        movl    -24(%rbp), %eax
+        addl    $2, %eax
+        sall    $3, %eax
+        movslq  %eax, %rdx
+        movq    -312(%rbp), %rcx
+        movq    -32(%rbp), %rax
+        movq    %rcx, %rsi
+        movq    %rax, %rdi
+        call    memcpy
+        movq    $0, -8(%rbp)
+        movq    $0, -16(%rbp)
+        movl    $0, -20(%rbp)
+.L34:
+        movq    -16(%rbp), %rax
+        movq    %rax, -40(%rbp)
+        movl    -20(%rbp), %edx
+        movq    -32(%rbp), %rax
+        movl    %edx, %esi
+        movq    %rax, %rdi
+        call    get_pointer_field
+        movq    %rax, -16(%rbp)
+        addl    $1, -20(%rbp)
+        movq    -40(%rbp), %rax
+        movq    %rax, %rdi
+        call    in_from_space
+        testl   %eax, %eax
+        je      .L33
+        movl    $1, %eax
+        leaq    0(,%rax,8), %rdx
+        movq    -40(%rbp), %rax
+        addq    %rax, %rdx
+        leaq    -304(%rbp), %rax
+        movq    %rax, %rsi
+        movq    %rdx, %rdi
+        call    set_bitmap
+        movl    -304(%rbp), %eax
+        cmpl    $1, %eax
+        jne     .L33
+        movq    -40(%rbp), %rax
+        addq    $16, %rax
+        movq    (%rax), %rax
+        movq    %rax, %rdi
+        call    in_to_space
+        testl   %eax, %eax
+        jne     .L33
+        movq    -40(%rbp), %rax
+        movq    %rax, -8(%rbp)
+.L33:
+        cmpq    $0, -16(%rbp)
+        jne     .L34
+        movq    -312(%rbp), %rax
+        leaq    16(%rax), %rdx
+        movq    -32(%rbp), %rax
+        movq    %rax, (%rdx)
+        movq    -8(%rbp), %rax
+        movq    %rax, -312(%rbp)
+.L32:
+        cmpq    $0, -312(%rbp)
+        jne     .L35
         nop
-        popq    %rbp
+        nop
+        leave
         ret
 forward:
         pushq   %rbp
         movq    %rsp, %rbp
-        subq    $24, %rsp
+        subq    $32, %rsp
         movq    %rdi, -24(%rbp)
-        movq    to_space(%rip), %rax
-        cmpq    %rax, -24(%rbp)
-        jnb     .L18
+        movq    -24(%rbp), %rax
+        movq    %rax, %rdi
+        call    in_to_space
+        testl   %eax, %eax
+        je      .L37
+        movq    -24(%rbp), %rax
+        jmp     .L38
+.L37:
         movq    -24(%rbp), %rax
         movl    $0, %esi
         movq    %rax, %rdi
-        call    getPointerField
+        call    get_pointer_field
         movq    %rax, -8(%rbp)
-        movq    to_space(%rip), %rax
-        cmpq    %rax, -8(%rbp)
-        jnb     .L19
+        cmpq    $0, -8(%rbp)
+        je      .L39
+        movq    -8(%rbp), %rax
+        movq    %rax, %rdi
+        call    in_to_space
+        testl   %eax, %eax
+        jne     .L39
         movq    -24(%rbp), %rax
         movq    %rax, %rdi
         call    chase
-.L19:
+.L39:
         movq    -8(%rbp), %rax
-        jmp     .L20
-.L18:
-        movq    -24(%rbp), %rax
-.L20:
+.L38:
         leave
         ret
-.LC2:
-        .string "Var %d = %d is pointer\n"
+.LC7:
+        .string "Var %d = %d (pointer)\n"
 visit_pointer_vars:
         pushq   %rbp
         movq    %rsp, %rbp
@@ -222,44 +441,62 @@ visit_pointer_vars:
         movq    %rsi, -32(%rbp)
         movq    %rdx, -40(%rbp)
         movl    $0, -4(%rbp)
-        jmp     .L22
-.L24:
+        jmp     .L41
+.L46:
         movl    -4(%rbp), %eax
         cltq
-        leaq    0(,%rax,4), %rdx
+        movq    -24(%rbp), %rdx
+        subq    %rax, %rdx
+        movq    %rdx, %rax
+        salq    $2, %rax
+        leaq    -4(%rax), %rdx
         movq    -32(%rbp), %rax
         addq    %rdx, %rax
         movl    (%rax), %eax
         movl    %eax, -8(%rbp)
         cmpl    $0, -8(%rbp)
-        je      .L23
+        je      .L42
         movl    -4(%rbp), %eax
+        negl    %eax
         cltq
-        subq    -24(%rbp), %rax
-        addq    $1, %rax
         leaq    0(,%rax,8), %rdx
         movq    -40(%rbp), %rax
         addq    %rdx, %rax
         movq    (%rax), %rax
         movq    %rax, -16(%rbp)
+        movq    from_space(%rip), %rax
+        cmpq    %rax, -16(%rbp)
+        jb      .L47
+        movq    to_space(%rip), %rax
+        cmpq    %rax, -16(%rbp)
+        jnb     .L47
         movq    -16(%rbp), %rdx
         movl    -4(%rbp), %eax
         movl    %eax, %esi
-        movl    $.LC2, %edi
+        movl    $.LC7, %edi
         movl    $0, %eax
         call    printf
         movq    -16(%rbp), %rax
         movq    %rax, %rdi
         call    forward
-.L23:
+        movl    -4(%rbp), %edx
+        negl    %edx
+        movslq  %edx, %rdx
+        leaq    0(,%rdx,8), %rcx
+        movq    -40(%rbp), %rdx
+        addq    %rcx, %rdx
+        movq    %rax, (%rdx)
+.L42:
         addl    $1, -4(%rbp)
-.L22:
+.L41:
         movl    -4(%rbp), %eax
         cltq
         cmpq    %rax, -24(%rbp)
-        jg      .L24
+        jg      .L46
+        jmp     .L40
+.L47:
         nop
-        nop
+.L40:
         leave
         ret
 visit_pointer_params:
@@ -270,9 +507,9 @@ visit_pointer_params:
         nop
         popq    %rbp
         ret
-.LC3:
+.LC8:
         .string "Num parameters: %d\n"
-.LC4:
+.LC9:
         .string "Num variables:  %d\n"
 scan_stack_frame:
         pushq   %rbp
@@ -314,12 +551,12 @@ scan_stack_frame:
         call    set_bitmap
         movq    -56(%rbp), %rax
         movq    %rax, %rsi
-        movl    $.LC3, %edi
+        movl    $.LC8, %edi
         movl    $0, %eax
         call    printf
         movq    -72(%rbp), %rax
         movq    %rax, %rsi
-        movl    $.LC4, %edi
+        movl    $.LC9, %edi
         movl    $0, %eax
         call    printf
         movq    -56(%rbp), %rax
@@ -378,8 +615,8 @@ scan_stack_frame:
         salq    $2, %rax
         movq    %rax, -104(%rbp)
         movl    $0, -64(%rbp)
-        jmp     .L27
-.L28:
+        jmp     .L50
+.L51:
         movl    -64(%rbp), %eax
         cltq
         movl    -368(%rbp,%rax,4), %ecx
@@ -388,14 +625,14 @@ scan_stack_frame:
         movslq  %edx, %rdx
         movl    %ecx, (%rax,%rdx,4)
         addl    $1, -64(%rbp)
-.L27:
+.L50:
         movl    -64(%rbp), %eax
         cltq
         cmpq    %rax, -56(%rbp)
-        jg      .L28
+        jg      .L51
         movl    $0, -60(%rbp)
-        jmp     .L29
-.L30:
+        jmp     .L52
+.L53:
         movl    -64(%rbp), %eax
         leal    1(%rax), %edx
         movl    %edx, -64(%rbp)
@@ -406,11 +643,11 @@ scan_stack_frame:
         movslq  %edx, %rdx
         movl    %ecx, (%rax,%rdx,4)
         addl    $1, -60(%rbp)
-.L29:
+.L52:
         movl    -60(%rbp), %eax
         cltq
         cmpq    %rax, -72(%rbp)
-        jg      .L30
+        jg      .L53
         movl    $4, %eax
         salq    $3, %rax
         negq    %rax
@@ -422,6 +659,8 @@ scan_stack_frame:
         movq    %rcx, %rsi
         movq    %rax, %rdi
         call    visit_pointer_vars
+        movl    $10, %edi
+        call    putchar
         movq    %rbx, %rsp
         nop
         leaq    -40(%rbp), %rsp
@@ -432,92 +671,94 @@ scan_stack_frame:
         popq    %r15
         popq    %rbp
         ret
-.LC5:
+.LC10:
         .string "Collecting Garbage:"
-.LC6:
-        .string "Done!"
 collect_garbage:
         pushq   %rbp
         movq    %rsp, %rbp
         subq    $16, %rsp
         movq    %rdi, -8(%rbp)
-        movl    $.LC5, %edi
+        movl    $.LC10, %edi
         call    puts
-        jmp     .L32
-.L33:
+        jmp     .L55
+.L56:
         movq    -8(%rbp), %rax
         movq    %rax, %rdi
         call    scan_stack_frame
         movq    -8(%rbp), %rax
         movq    (%rax), %rax
         movq    %rax, -8(%rbp)
-.L32:
+.L55:
         cmpq    $0, -8(%rbp)
-        jne     .L33
-        movl    $.LC6, %edi
-        call    puts
+        jne     .L56
+        movq    current_to_space_pointer(%rip), %rax
+        movq    %rax, current_heap_pointer(%rip)
         nop
         leave
         ret
-.LC7:
+.LC11:
         .string "Out of memory\n"
 allocate_heap:
         pushq   %rbp
         movq    %rsp, %rbp
-        subq    $32, %rsp
+        subq    $16, %rsp
         movq    %rdi, -8(%rbp)
         movq    %rsi, -16(%rbp)
-        movq    %rdx, -24(%rbp)
         movq    heap_pointer(%rip), %rax
         movq    %rax, from_space(%rip)
         movq    from_space(%rip), %rax
-        movq    -16(%rbp), %rdx
+        movq    heap_size(%rip), %rdx
         salq    $3, %rdx
         addq    %rdx, %rax
         movq    %rax, to_space(%rip)
         movq    current_heap_pointer(%rip), %rdx
         movq    to_space(%rip), %rax
         cmpq    %rax, %rdx
-        jb      .L35
+        jb      .L58
         movq    to_space(%rip), %rdx
         movq    from_space(%rip), %rax
         movq    %rdx, %rsi
         movq    %rax, %rdi
         call    swap
-.L35:
+.L58:
+        movq    to_space(%rip), %rax
+        movq    %rax, current_to_space_pointer(%rip)
         movq    current_heap_pointer(%rip), %rax
         movq    -8(%rbp), %rdx
         salq    $3, %rdx
         leaq    (%rax,%rdx), %rcx
         movq    from_space(%rip), %rax
-        movq    -16(%rbp), %rdx
+        movq    heap_size(%rip), %rdx
         salq    $3, %rdx
         addq    %rdx, %rax
         cmpq    %rax, %rcx
-        jbe     .L36
-        movq    -24(%rbp), %rax
+        jbe     .L59
+        movl    $0, %eax
+        call    print_heap
+        movq    -16(%rbp), %rax
         movq    %rax, %rdi
         call    collect_garbage
-.L36:
+        movl    $0, %eax
+        call    print_heap
         movq    current_heap_pointer(%rip), %rax
         movq    -8(%rbp), %rdx
         salq    $3, %rdx
         leaq    (%rax,%rdx), %rcx
-        movq    from_space(%rip), %rax
-        movq    -16(%rbp), %rdx
+        movq    to_space(%rip), %rax
+        movq    heap_size(%rip), %rdx
         salq    $3, %rdx
         addq    %rdx, %rax
         cmpq    %rax, %rcx
-        jbe     .L37
+        jbe     .L59
         movq    stderr(%rip), %rax
         movq    %rax, %rcx
         movl    $14, %edx
         movl    $1, %esi
-        movl    $.LC7, %edi
+        movl    $.LC11, %edi
         call    fwrite
         movl    $1, %edi
         call    exit
-.L37:
-        movq    heap_pointer(%rip), %rax
+.L59:
+        movq    current_heap_pointer(%rip), %rax
         leave
         ret
