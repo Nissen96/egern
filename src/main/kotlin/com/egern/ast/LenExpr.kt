d871ae84965f3fdfc9bb4815104320ebc8ef5ca8
Alexander Nissen <alexander@feldbech.dk>
2020-05-25 09:38:46 +0200

    For in loop (#35)
    * Add for-in loop as syntactic sugar

--- src/main/kotlin/com/egern/ast/LenExpr.kt
+++ src/main/kotlin/com/egern/ast/LenExpr.kt
@@ -2,7 +2,7 @@ package com.egern.ast
 
 import com.egern.visitor.Visitor
 
-class LenExpr(val expr: Expr, lineNumber: Int, charPosition: Int) : Expr(lineNumber, charPosition) {
+class LenExpr(val expr: Expr, lineNumber: Int = -1, charPosition: Int = -1) : Expr(lineNumber, charPosition) {
     override fun accept(visitor: Visitor) {
         visitor.preVisit(this)
         expr.accept(visitor)
