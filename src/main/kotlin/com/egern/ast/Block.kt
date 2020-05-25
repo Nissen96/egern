c1fe03a7a58185896698c0d9caa7d0635dcc4237
Alexander Nissen <alexander@feldbech.dk>
2020-05-25 11:44:37 +0200

    Accidentally changed a var to val - now fixed

--- src/main/kotlin/com/egern/ast/Block.kt
+++ src/main/kotlin/com/egern/ast/Block.kt
@@ -4,7 +4,7 @@ import com.egern.symbols.SymbolTable
 import com.egern.visitor.Visitor
 
 class Block(
-    val stmts: List<ASTNode>,
+    var stmts: List<ASTNode>,
     lineNumber: Int = -1,
     charPosition: Int = -1
 ) : ASTNode(lineNumber, charPosition) {
